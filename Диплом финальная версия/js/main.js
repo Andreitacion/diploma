1c5dad3ff77ed2ee18346b30958199214f1c54eb
Andreitacion <131300124+Andreitacion@users.noreply.github.com>
2023-11-10 13:40:36 +0300

    Add files via upload

---
+++ Диплом финальная версия/js/main.js
@@ -0,0 +1,63 @@
+$(function () {
+
+  $('.testimonials__slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: $('.testimonials__dots'),
+    waitForAnimate: false,
+    // responsive:
+    //   [
+    //     {
+    //       breakpoint: 700,
+    //       settings: {
+
+    //       },
+    //     },
+    //   ]
+  })
+  $('.testimonials__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickPrev')
+  })
+  $('.testimonials__next').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickNext')
+  })
+
+  $('.program__acc-link').on('click', function (e) {
+    e.preventDefault()
+    if ($(this).hasClass('program__acc-link--active')) {
+      $(this).removeClass('program__acc-link--active')
+      $(this).children('.program__acc-text').slideUp()
+    } else {
+      $('.program__acc-link').removeClass('program__acc-link--active')
+      $('.program__acc-text').slideUp()
+      $(this).addClass('program__acc-link--active')
+      $(this).children('.program__acc-text').slideDown()
+    }
+  })
+
+  $(".header__nav-list a, .header__top-btn, .footer__go-top").on("click", function (e) {
+    e.preventDefault()
+    var id = $(this).attr('href'),
+      top = $(id).offset().top
+    $('body,html').animate({ scrollTop: top }, 800)
+  })
+
+  setInterval(() => {
+    if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }
+  }, 0)
+  $('.burger, .overlay, .header__top a').on('click', function (e) {
+    e.preventDefault()
+    $('.header__top').toggleClass('header__top--open')
+    $('.overlay').toggleClass('overlay--show')
+  })
+
+  $('.footer__top-title--slide').on('click', function () {
+    $(this).next().slideToggle()
+  })
+})
